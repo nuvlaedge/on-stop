d88bf80b519bf9d41b84c0d26021f6f296a4cc11
Cristovao Cordeiro <cris@sixsq.com>
2021-03-02 15:58:12 +0100

    remove node label

--- code/run.py
+++ code/run.py
@@ -25,5 +25,42 @@ if len(sys.argv) > 1 and "paused".startswith(sys.argv[1].lower()):
 
     myself.pause()
 else:
-    logging.info('to be implemented')
+    logging.info('Starting NuvlaBox deep cleanup')
+
+    info = docker_client.info()
+
+    swarm_info = info.get('Swarm', {})
+
+    node_id = swarm_info.get('NodeID')
+
+    local_node_state = swarm_info.get('LocalNodeState', 'inactive')
+
+    is_swarm_enabled = True if node_id and local_node_state != "inactive" else False
+
+    remote_managers = [rm.get('NodeID') for rm in swarm_info.get('RemoteManagers', [])]
+    i_am_manager = True if node_id in remote_managers else False
+
+    if i_am_manager:
+        cluster_nodes = docker_client.nodes.list()
+
+        # remove label
+        label = 'nuvlabox'
+        node = docker_client.nodes.get(node_id)
+        node_spec = node.attrs['Spec']
+        node_labels = node_spec.get('Labels', {})
+        node_labels.pop(label)
+        node_spec['Labels'] = node_labels
+        logging.info(f'Removing node label {label} from this node ({node_id})')
+        node.update(node_spec)
+
+        # if len(cluster_nodes) > 1:
+
+
+
+
+    # if manager:
+    # check if node has nuvlabox label and remove that label
+
+
+    # if in cluster mode, manager, and last node, also get rid of the ack and DG services/networks (also the traefik service) + nbmosquitto service as well
 
